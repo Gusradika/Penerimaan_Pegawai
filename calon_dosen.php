9819dd4613ae9a17eace28c0300181551449629c
Aditya Kesuma <aditezo24@gmail.com>
2022-11-22 22:17:03 +0700

    Saya ngepush file error

--- calon_dosen.php
+++ calon_dosen.php
@@ -142,47 +142,17 @@
                                         <td><?php echo $no++ ?></td>
                                         <td><?php echo $row['nama'] ?></td>
                                         <td><?php echo $row['jenis_kelamin'] ?></td>
-                                        <td><?php echo $row['tempat_lahir'];
-                                                ['tanggal_lahir'] ?></td>
-                                        <td><?php echo $row['alamat'] ?></td>
-                                        <td><?php echo $row['agama'] ?></td>
-                                        <td><?php echo $row['status'] ?></td>
-                                        <td><?php echo $row['pendidikan_terakhir'] ?></td>
-                                        <td>
-                                            <div class="btn-group">
-                                                <a href="#" class="btn btn-primary">Ubah </a>&nbsp;&nbsp;
-                                                <a href="#" class="btn btn-warning">Hapus</a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <?php } ?>
-                                </tbody>
-                            </table>
 
-                            <!-- sdjkasdjkasdjasdjlkasdjklzdfjkldfjkldfjkldfjkl -->
+                                        </footer>
                         </div>
                     </div>
-                </div>
-            </main>
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
-    <script src="js/scripts.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-    <script src="js/datatables-simple-demo.js"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                        crossorigin="anonymous">
+                    </script>
+                    <script src="js/scripts.js"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous">
+                    </script>
+                    <script src="js/datatables-simple-demo.js"></script>
 </body>
 
 </html>
